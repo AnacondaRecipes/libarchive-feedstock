0c887f64ba58ed0dcc79b42a01b63d250cdd40e1
Ray Donnelly <mingw.android@gmail.com>
2017-11-02 11:08:48 +0000

    Update to 3.3.2, use cross-compilers, libiconv only needed for macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,11 @@
 # Needed for the tests.
 export CFLAGS="-std=c99 ${CFLAGS}"
 
-if [ "`uname`" == 'Darwin' ]
-then
-    export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-else
-    export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 fi
 
+<<<<<<< HEAD
 autoreconf -ivf
 ./configure --prefix=${PREFIX} \
             --with-expat \
@@ -19,4 +17,14 @@ autoreconf -ivf
             --without-xml2
 make -j${CPU_COUNT}
 #eval ${LIBRARY_SEARCH_VAR}="${PREFIX}/lib" make check
+=======
+autoreconf -vfi
+./configure --prefix=${PREFIX}  \
+            --with-expat        \
+            --without-nettle    \
+            --without-lz4       \
+            --without-lzmadec   \
+            --without-xml2
+make -j${CPU_COUNT} ${VERBOSE_AT}
+>>>>>>> Update to 3.3.2, use cross-compilers, libiconv only needed for macOS
 make install
